a94c4b2920b9d64468a2e47b4194ac223b86a7f0
KoonaSainath <sainathwnp@gmail.com>
2022-12-31 18:29:33 +0530

    Initial commit

---
+++ Sainath.E-Commerce.BooksForSale.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sainath.E_Commerce.BooksForSale.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
