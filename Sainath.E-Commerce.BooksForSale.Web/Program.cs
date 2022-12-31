baec743ea8133e99505c5453fd5090864aa98080
KoonaSainath <sainathwnp@gmail.com>
2022-12-31 19:16:55 +0530

    Set up dependency injection for DbContext by using sql server and connection string from appsettings.json. Also installed nuget packages.

--- Sainath.E-Commerce.BooksForSale.Web/Program.cs
+++ Sainath.E-Commerce.BooksForSale.Web/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Sainath.E_Commerce.BooksForSale.DataAccess.DbContextClasses;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<BooksForSaleDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BooksForSaleConnectionString"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
