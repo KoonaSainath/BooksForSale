0be4763b2d8d4f5bb797dd14f97c629b66bcbfd6
KoonaSainath <sainathwnp@gmail.com>
2023-01-01 13:10:46 +0530

    Created IRepository generic interface and added generic db methods

---
+++ Sainath.E-Commerce.BooksForSale.DataAccess/IRepositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sainath.E_Commerce.BooksForSale.DataAccess.IRepositories
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAllRecords();
+        T GetRecord(int id);
+        T GetRecord(Expression<Func<T, bool>> expression);
+        void InsertRecord(T record);
+        void RemoveRecord(T record);
+        void RemoveRecords(IEnumerable<T> records);
+    }
+}
