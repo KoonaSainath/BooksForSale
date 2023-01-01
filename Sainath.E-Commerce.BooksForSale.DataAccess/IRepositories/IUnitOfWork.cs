0c0c83020969ec1d3f5597dd3ea267996d23048f
KoonaSainath <sainathwnp@gmail.com>
2023-01-01 14:27:18 +0530

    UnitOfWork and IUnitOfWork set up completed

---
+++ Sainath.E-Commerce.BooksForSale.DataAccess/IRepositories/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sainath.E_Commerce.BooksForSale.DataAccess.IRepositories
+{
+    public interface IUnitOfWork
+    {
+        ICategoryRepository CategoryRepository { get; }
+        void Save();
+    }
+}
