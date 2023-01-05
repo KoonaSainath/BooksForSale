4971f312d3416a336fd56e61f6129a0013a81620
KoonaSainath <sainathwnp@gmail.com>
2023-01-05 22:18:11 +0530

    HttpPost method implemented for creating category

--- Sainath.E-Commerce.BooksForSale.Models/ViewModels/Category.cs
+++ Sainath.E-Commerce.BooksForSale.Models/ViewModels/Category.cs
@@ -22,7 +22,7 @@ namespace Sainath.E_Commerce.BooksForSale.Models.ViewModels
         [Required(ErrorMessage = "Please enter display order.")]
         [Range(minimum: 1, maximum: 100, ErrorMessage = "Display order must be between 1 and 100, both included.")]
         [Display(Name = "Display order")]
-        public int DisplayOrder { get; set; }
+        public int? DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
         public DateTime UpdatedDateTime { get; set; } = DateTime.Now;
     }
--- Sainath.E-Commerce.BooksForSale.Web/Areas/Admin/Controllers/CategoryController.cs
+++ Sainath.E-Commerce.BooksForSale.Web/Areas/Admin/Controllers/CategoryController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.Localization;
 using Sainath.E_Commerce.BooksForSale.Models.ViewModels;
 using Sainath.E_Commerce.BooksForSale.Web.Configurations.IConfigurations;
+using System.Net.Http.Headers;
 using System.Runtime.CompilerServices;
 
 namespace Sainath.E_Commerce.BooksForSale.Web.Areas.Admin.Controllers
@@ -20,7 +21,7 @@ namespace Sainath.E_Commerce.BooksForSale.Web.Areas.Admin.Controllers
             string baseAddress = configuration.BaseAddressForWebApi;
             HttpClient httpClient = new HttpClient();
             httpClient.DefaultRequestHeaders.Accept.Clear();
-            httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             httpClient.BaseAddress = new Uri(baseAddress);
             HttpResponseMessage response = await httpClient.GetAsync(requestUrl);
             Task<IEnumerable<Category>> categoriesTask = response.Content.ReadFromJsonAsync<IEnumerable<Category>>();
@@ -28,8 +29,27 @@ namespace Sainath.E_Commerce.BooksForSale.Web.Areas.Admin.Controllers
             return View(categories);
         }
         [HttpGet]
-        public async Task<IActionResult> CreateCategory()
+        public async Task<IActionResult> InsertCategory()
         {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> InsertCategory(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                string requestUrl = "api/Category/POST/InsertCategory";
+                HttpClient httpClient = new HttpClient();
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                httpClient.BaseAddress = new Uri(configuration.BaseAddressForWebApi);
+                HttpResponseMessage response = await httpClient.PostAsJsonAsync<Category>(requestUrl, category);
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index", "Category");
+                }
+            }
             return View();
         }
     }
