6a1cf96386403949b2999bfb12bae1731bf78889
KoonaSainath <sainathwnp@gmail.com>
2023-01-07 12:59:44 +0530

    Repository pattern set up for CoverType model

---
+++ Sainath.E-Commerce.BooksForSale.DataAccess/IRepositories/ICoverTypeRepository.cs
@@ -0,0 +1,14 @@
+﻿using Sainath.E_Commerce.BooksForSale.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sainath.E_Commerce.BooksForSale.DataAccess.IRepositories
+{
+    public interface ICoverTypeRepository : IRepository<CoverType>
+    {
+        void UpdateCoverType(CoverType coverType);
+    }
+}
