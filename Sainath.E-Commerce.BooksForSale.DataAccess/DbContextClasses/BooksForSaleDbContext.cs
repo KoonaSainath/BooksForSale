6060c825b9b2a6fb134d0c251271b8c1775e3e3b
KoonaSainath <sainathwnp@gmail.com>
2023-01-22 19:29:07 +0530

    Book model created. Also added its migration and created Books table in the database

--- Sainath.E-Commerce.BooksForSale.DataAccess/DbContextClasses/BooksForSaleDbContext.cs
+++ Sainath.E-Commerce.BooksForSale.DataAccess/DbContextClasses/BooksForSaleDbContext.cs
@@ -16,5 +16,6 @@ namespace Sainath.E_Commerce.BooksForSale.DataAccess.DbContextClasses
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<CoverType> CoverTypes { get; set; }
+        public DbSet<Book> Books { get; set; }
     }
 }
