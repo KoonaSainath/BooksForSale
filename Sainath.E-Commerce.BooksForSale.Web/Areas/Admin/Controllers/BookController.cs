febc720549c14fdb305b67497ec599a3c4a0500e
KoonaSainath <sainathwnp@gmail.com>
2023-01-22 19:35:08 +0530

    Created BooksController

---
+++ Sainath.E-Commerce.BooksForSale.Web/Areas/Admin/Controllers/BookController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sainath.E_Commerce.BooksForSale.Web.Areas.Admin.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
