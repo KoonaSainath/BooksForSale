61912669466ea9a81eedc4a2dda53949a72042e6
KoonaSainath <sainathwnp@gmail.com>
2023-01-23 22:22:55 +0530

    Created BookController

--- Sainath.E-Commerce.BooksForSale.Web/Areas/Admin/Controllers/BookController.cs
+++ Sainath.E-Commerce.BooksForSale.Web/Areas/Admin/Controllers/BookController.cs
@@ -1,10 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Sainath.E_Commerce.BooksForSale.Models.ViewModels;
 
 namespace Sainath.E_Commerce.BooksForSale.Web.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class BookController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult UpsertBook()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult UpsertBook(Book book)
         {
             return View();
         }
