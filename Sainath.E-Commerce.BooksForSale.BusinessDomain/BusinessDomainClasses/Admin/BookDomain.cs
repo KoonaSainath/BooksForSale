eb8f84b4aae684743dd14ec40723eb7211f0ea2a
KoonaSainath <sainathwnp@gmail.com>
2023-01-24 16:48:10 +0530

    Created BookDomain and implemented all required domain methods

---
+++ Sainath.E-Commerce.BooksForSale.BusinessDomain/BusinessDomainClasses/Admin/BookDomain.cs
@@ -0,0 +1,53 @@
+﻿using Sainath.E_Commerce.BooksForSale.DataAccess.DataAccessClasses.Admin;
+using Sainath.E_Commerce.BooksForSale.DataAccess.IRepositories;
+using Sainath.E_Commerce.BooksForSale.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sainath.E_Commerce.BooksForSale.BusinessDomain.BusinessDomainClasses.Admin
+{
+    public class BookDomain
+    {
+        private readonly BookData bookData;
+        public BookDomain(IUnitOfWork unitOfWork)
+        {
+            bookData = new BookData(unitOfWork);
+        }
+        public IEnumerable<Book> GetAllBooks()
+        {
+            IEnumerable<Book> allBooks = bookData.GetAllBooks();
+            return allBooks;
+        }
+        public Book GetBook(int id)
+        {
+            Book book = bookData.GetBook(id);
+            return book;
+        }
+        public Book GetBook(Expression<Func<Book, bool>> expression)
+        {
+            Book book = bookData.GetBook(expression);
+            return book;
+        }
+        public void InsertBook(Book book)
+        {
+            bookData.InsertBook(book);
+        }
+        public void RemoveBook(Book book)
+        {
+            bookData.RemoveBook(book);
+        }
+        public void RemoveBooks(IEnumerable<Book> books)
+        {
+            bookData.RemoveBooks(books);
+        }
+        public Book UpdateBook(Book book)
+        {
+            bookData.UpdateBook(book);
+            return book;
+        }
+    }
+}
