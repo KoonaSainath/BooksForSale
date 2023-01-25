0bbb8a706f4f95630531659db2b626b10a49cc1f
KoonaSainath <sainathwnp@gmail.com>
2023-01-25 21:36:36 +0530

    Package change for SelectListItem

--- Sainath.E-Commerce.BooksForSale.Models/ViewModels/BookVM.cs
+++ Sainath.E-Commerce.BooksForSale.Models/ViewModels/BookVM.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web.Mvc;
 
 namespace Sainath.E_Commerce.BooksForSale.Models.ViewModels
 {
