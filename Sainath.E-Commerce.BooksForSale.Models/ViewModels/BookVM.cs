7ced3dcd6efc20778e6f61e45651d4f3cf991a69
KoonaSainath <sainathwnp@gmail.com>
2023-01-25 21:32:17 +0530

    Created BookVM to hold list categories and covertypes for the dropdown along with the Book model

---
+++ Sainath.E-Commerce.BooksForSale.Models/ViewModels/BookVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Sainath.E_Commerce.BooksForSale.Models.ViewModels
+{
+    public class BookVM
+    {
+        public Book Book { get; set; }
+        public IEnumerable<SelectListItem> Categories { get; set; }
+        public IEnumerable<SelectListItem> CoverTypes { get; set; }
+    }
+}
