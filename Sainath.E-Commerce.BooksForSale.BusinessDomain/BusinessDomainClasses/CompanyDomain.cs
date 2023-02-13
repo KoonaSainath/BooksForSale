29e609e8a5cd80765cc5f8bca22fddb6637f537e
KoonaSainath <sainathwnp@gmail.com>
2023-02-13 21:45:35 +0530

    Implemented Domain layer for Company model

---
+++ Sainath.E-Commerce.BooksForSale.BusinessDomain/BusinessDomainClasses/CompanyDomain.cs
@@ -0,0 +1,50 @@
+﻿using Sainath.E_Commerce.BooksForSale.DataAccess.DataAccessClasses.Admin;
+using Sainath.E_Commerce.BooksForSale.DataAccess.IRepositories;
+using Sainath.E_Commerce.BooksForSale.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sainath.E_Commerce.BooksForSale.BusinessDomain.BusinessDomainClasses
+{
+    public class CompanyDomain
+    {
+        private readonly CompanyData companyData;
+        public CompanyDomain(IUnitOfWork unitOfWork)
+        {
+            companyData = new CompanyData(unitOfWork);
+        }
+
+        public IEnumerable<Company> GetAllCompanies()
+        {
+            return companyData.GetAllCompanies();
+        }
+
+        public Company GetCompany(int companyId)
+        {
+            return companyData.GetCompany(companyId);
+        }
+
+        public void InsertCompany(Company company)
+        {
+            companyData.InsertCompany(company);
+        }
+
+        public void RemoveCompany(Company company)
+        {
+            companyData.RemoveCompany(company);
+        }
+
+        public void RemoveCompanies(IEnumerable<Company> companies)
+        {
+            companyData.RemoveCompanies(companies);
+        }
+
+        public void UpdateCompany(Company company)
+        {
+            companyData.UpdateCompany(company);
+        }
+    }
+}
