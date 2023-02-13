80f6f51ce28127e4090428c6ba1defd73484c728
KoonaSainath <sainathwnp@gmail.com>
2023-02-13 21:41:25 +0530

    Implemented CompanyData class acting as DataAccess layer for Company model

---
+++ Sainath.E-Commerce.BooksForSale.DataAccess/DataAccessClasses/Admin/CompanyData.cs
@@ -0,0 +1,54 @@
+﻿using Sainath.E_Commerce.BooksForSale.DataAccess.BaseClasses;
+using Sainath.E_Commerce.BooksForSale.DataAccess.IRepositories;
+using Sainath.E_Commerce.BooksForSale.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sainath.E_Commerce.BooksForSale.DataAccess.DataAccessClasses.Admin
+{
+    public class CompanyData : BaseData
+    {
+        public CompanyData(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+
+        }
+        
+        public IEnumerable<Company> GetAllCompanies()
+        {
+            return unitOfWork.CompanyRepository.GetAllRecords();
+        }
+
+        public Company GetCompany(int companyId)
+        {
+            Company company = unitOfWork.CompanyRepository.GetRecord(companyId);
+            return company;
+        }
+
+        public void InsertCompany(Company company)
+        {
+            unitOfWork.CompanyRepository.InsertRecord(company);
+            unitOfWork.Save();
+        }
+
+        public void RemoveCompany(Company company)
+        {
+            unitOfWork.CompanyRepository.RemoveRecord(company);
+            unitOfWork.Save();
+        }
+
+        public void RemoveCompanies(IEnumerable<Company> companies)
+        {
+            unitOfWork.CompanyRepository.RemoveRecords(companies);
+            unitOfWork.Save();
+        }
+
+        public void UpdateCompany(Company company)
+        {
+            unitOfWork.CompanyRepository.UpdateCompany(company);
+            unitOfWork.Save();
+        }
+    }
+}
