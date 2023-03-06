918b29e2e4ad4cc137a325fa1219139bd42fe849
KoonaSainath <sainathwnp@gmail.com>
2023-03-06 20:25:30 +0530

    Create manage orders page

---
+++ Sainath.E-Commerce.BooksForSale.Web/Areas/Customer/Controllers/ManageOrders.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sainath.E_Commerce.BooksForSale.Web.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class ManageOrders : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
