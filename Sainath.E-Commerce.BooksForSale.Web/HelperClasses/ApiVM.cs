6cd27fa53a82c9b824edee9b8d651c52f654f6be
KoonaSainath <sainathwnp@gmail.com>
2023-03-18 18:01:55 +0530

    Simplified web api calls by creating a common class InvokeApi.cs for all api requests. Implemented this simplified functionality for Categories page

---
+++ Sainath.E-Commerce.BooksForSale.Web/HelperClasses/ApiVM.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Sainath.E_Commerce.BooksForSale.Web.HelperClasses
+{
+    public class ApiVM<T> where T : class
+    {
+        public T TObject { get; set; }
+        public HttpResponseMessage Response { get; set; }
+    }
+}
