e6061fe1d49a3c08f1a9a07c630d08b0138ceafd
KoonaSainath <sainathwnp@gmail.com>
2023-03-25 13:32:34 +0530

    Created nunit test project and implemented test methods for BookController

---
+++ Sainath.E-Commerce.BooksForSale.NUnitTest/Admin/BookControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using Sainath.E_Commerce.BooksForSale.Web.Areas.Admin.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sainath.E_Commerce.BooksForSale.NUnitTest.Admin
+{
+    [TestFixture]
+    public class BookControllerTest : BaseTest<BookController>
+    {
+        [Test]
+        public void Book_Index()
+        {
+            ViewResult viewResult = UtControllerInstance.Index() as ViewResult;
+            Assert.IsNotNull(viewResult.ViewData);
+        }
+
+        [Test]
+        public void Book_GetAllBooksApiEndPoint()
+        {
+            JsonResult jsonResult = UtControllerInstance.GetAllBooksApiEndPoint().GetAwaiter().GetResult() as JsonResult;
+            Assert.IsNotNull(jsonResult.Value);
+        }
+    }
+}
---
+++ Sainath.E-Commerce.BooksForSale.NUnitTest/BaseTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Sainath.E_Commerce.BooksForSale.Web;
+using Sainath.E_Commerce.BooksForSale.Web.Areas.Admin.Controllers;
+using Sainath.E_Commerce.BooksForSale.Web.Configurations;
+using Sainath.E_Commerce.BooksForSale.Web.Configurations.IConfigurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sainath.E_Commerce.BooksForSale.NUnitTest
+{
+    public class BaseTest<T> where T : Controller
+    {
+        public Mock<IBooksForSaleConfiguration> mockBooksForSaleConfiguration { get; set; }
+        public Mock<IWebHostEnvironment> mockWebHostEnvironment { get; set; }
+        public IBooksForSaleConfiguration booksForSaleConfiguration { get; set; }
+        public IWebHostEnvironment webHostEnvironment { get; set; }
+        public T UtControllerInstance 
+        {
+            get
+            {
+                mockBooksForSaleConfiguration = new Mock<IBooksForSaleConfiguration>();
+                mockBooksForSaleConfiguration.SetupProperty(x => x.BaseAddressForWebApi, "https://localhost:7138");
+                mockBooksForSaleConfiguration.SetupProperty(x => x.BaseAddressForWebApplication, "https://localhost:7109/");
+                this.booksForSaleConfiguration = mockBooksForSaleConfiguration.Object;
+
+                mockWebHostEnvironment = new Mock<IWebHostEnvironment>();
+                this.webHostEnvironment = mockWebHostEnvironment.Object;
+                if (typeof(T) == typeof(BookController))
+                {
+                    T t = (T) Activator.CreateInstance(typeof(T), booksForSaleConfiguration, webHostEnvironment);
+                    return t;
+                }
+                else
+                {
+                    T t = (T)Activator.CreateInstance(typeof(T));
+                    return t;
+                }
+            }
+        }
+    }
+}
